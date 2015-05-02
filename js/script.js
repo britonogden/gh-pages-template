0879f23734f59d79104f1c2267dd55badebc2f1f
strategicwebsite <sapwebsite45@gmail.com>
2015-05-01 22:38:10 -0700

    trying to push

---
+++ js/script.js
@@ -0,0 +1,75 @@
+		onload="preloadImages();"
+		
+		img1 = newImage("img/1a.jpg");
+		img2 = newImage("img/1a.jpg");
+		img3 = newImage("img/visions.jpg");
+		img4 = newImage("img/123.jpg");
+		
+		function changeImage (newimage) {
+			document['visions'].src = newimage;
+		}
+		
+		function changeImage1 (newimage) {
+			document['visions1'].src = newimage;
+		}
+		
+		function changeImage2 (newimage) {
+			document['visions2'].src = newimage;
+		}
+		
+		function changeImage3 (newimage) {
+			document['visions3'].src = newimage;
+		}
+		
+		function changeImage4 (newimage) {
+			document['visions4'].src = newimage;
+		}
+		
+		function changeImage5 (newimage) {
+			document['visions5'].src = newimage;
+		}
+
+		function changeImage6 (newimage) {
+			document['visions6'].src = newimage;
+		}
+		
+		function changeImage7 (newimage) {
+			document['visions7'].src = newimage;
+		}
+		
+		function changeImage8 (newimage) {
+			document['visions8'].src = newimage;
+		}
+		
+		function changeImage9 (newimage) {
+			document['visions9'].src = newimage;
+		}
+		
+		function changeImage10 (newimage) {
+			document['visions10'].src = newimage;
+		}
+		
+		function changeImage11 (newimage) {
+			document['visions11'].src = newimage;
+		}
+		
+		function changeImage12 (newimage) {
+			document['visions12'].src = newimage;
+		}
+		
+		function changeImage13 (newimage) {
+			document['visions13'].src = newimage;
+		}
+		
+		function changeImage14 (newimage) {
+			document['visions14'].src = newimage;
+		}
+				function changeImage15 (newimage) {
+			document['visions15'].src = newimage;
+		}
+				function changeImage16 (newimage) {
+			document['visions16'].src = newimage;
+		}
+				function changeImage17 (newimage) {
+			document['visions17'].src = newimage;
+		}
